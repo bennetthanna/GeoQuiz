db3f547562a70286ad7e30a6e9e515685d8bfef5
Hanna Bennett <bennetthanna22@gmail.com>
2017-09-23 23:41:40 -0700

    Customized toast to show at the top instead of the bottom of the screen

--- app/src/main/java/com/hannabennett/geoquiz/QuizActivity.java
+++ app/src/main/java/com/hannabennett/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.hannabennett.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,18 +21,22 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                               R.string.correct_toast,
-                               Toast.LENGTH_SHORT).show();
+                Toast correct_toast = Toast.makeText(QuizActivity.this,
+                                                     R.string.correct_toast,
+                                                     Toast.LENGTH_SHORT);
+                correct_toast.setGravity(Gravity.TOP,0,200);
+                correct_toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                               R.string.incorrect_toast,
-                               Toast.LENGTH_SHORT).show();
+                Toast incorrect_toast = Toast.makeText(QuizActivity.this,
+                                                       R.string.incorrect_toast,
+                                                       Toast.LENGTH_SHORT);
+                incorrect_toast.setGravity(Gravity.TOP,0,200);
+                incorrect_toast.show();
             }
         });
     }
