3cb672312d5d0bc62b11ff531faed8cd397c5556
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-02 11:53:27 -0700

    Users can no longer rotate CheatActivity after they cheat to clear out the cheating result Passed a mHasCheated Boolean to the savedInstanceState to be saved after rotation

--- app/src/main/java/com/hannabennett/geoquiz/CheatActivity.java
+++ app/src/main/java/com/hannabennett/geoquiz/CheatActivity.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_CHEATER = "cheater";
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.hannabennett.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.hannabennett.geoquiz.answer_shown";
@@ -17,6 +21,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
+    private Boolean mHasCheated;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -33,6 +38,11 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        if (savedInstanceState != null) {
+            mHasCheated = savedInstanceState.getBoolean(KEY_CHEATER, false);
+            setAnswerShownResult(mHasCheated);
+        }
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
@@ -47,6 +57,7 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                mHasCheated = true;
             }
         });
     }
@@ -55,5 +66,12 @@ public class CheatActivity extends AppCompatActivity {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_CHEATER, mHasCheated);
     }
 }
